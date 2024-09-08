8e1b3d1ce1cb68f6167961a93654e3d9aaca022e
FreeNewMan <lutovp@yandex.ru>
2024-09-08 22:19:42 +0500

    recode

--- src/js/__tests__/Character.js
+++ src/js/__tests__/Character.js
@@ -1,16 +1,5 @@
 import Character from "../Character";
 
-// const actors = [
-//   {name: 'Bowerman', type: 'Bowerman', health: 100, level: 1, attack: 25, defence: 25}, 
-//   {name: 'Swordsman', type: 'Swordsman', health: 100, level: 1, attack: 40, defence: 10}, 
-//   {name: 'Magician', type: 'Magician', health: 100, level: 1, attack: 10, defence: 40}, 
-//   {name: 'Undead', type: 'Undead', health: 100, level: 1, attack: 25, defence: 25}, 
-//   {name: 'Zombie', type: 'Zombie', health: 100, level: 1, attack: 40, defence: 10}, 
-//   {name: 'Daemon', type: 'Daemon', health: 100, level: 1, attack: 10, defence: 40},  
-// ];
-
-
-
 describe('Проверка класса Character', () => { 
   test('Проверка длины имени наименьшее ', () => {
     expect(() => new Character('B','Bowerman')).toThrowError("Имя игрока должно быть длиной от 2 до 10 символов")
